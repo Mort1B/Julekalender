24b0921aa08f0c8f849904b61b3fb92b845a3dca
Morten Bergseng <bergseng.m@gmail.com>
2020-12-01 17:25:17 +0100

    fixed

--- day1.js
+++ day1.js
@@ -1,6 +1,11 @@
-const arr = []
+// Find the missing number in a random list of numbers between 1 and 100.000.
 
+const arr = [] // Array was copied from the calendar
+
+//  min and max of the array
 let [min, max] = [Math.min(...arr), Math.max(...arr)];
+
+// Counts through the array and filters out the number missing from the list
 let out = Array.from(Array(max-min), (v,i) => i + min).filter(i => !arr.includes(i))
 
 // 81273
