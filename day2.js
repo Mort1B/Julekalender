a36279d4ecc9d220da7737f6aef07bcd39ff2515
Morten Bergseng <bergseng.m@gmail.com>
2020-12-08 09:49:06 +0100

    Update day2.js

--- day2.js
+++ day2.js
@@ -3,21 +3,21 @@ const { promises } = require("fs");
 
 const citizens = 5433000;
 const num = '7';
-let deliveroo = [];
+let delivery = [];
 let counter = 0;
 let primeNumber = 0;
 
 function isPrime(num) {
-    for(var i = 2; i < num; i++)
+    for(let i = 2; i < num; i++)
       if(num % i === 0) return false;
     return true;
 }
   
 for (i = 0; i <= citizens; i++) {
-    var alphanumeric = i.toString();
+    let stringy = i.toString();
     
-    if (alphanumeric.includes(num)) {
-        for (j = primeNumber; j <= i; j++) {
+    if (stringy.includes(num)) {
+        for (let j = primeNumber; j <= i; j++) {
             if (isPrime(j)) {
                 primeNumber = j;
             }
@@ -25,8 +25,8 @@ for (i = 0; i <= citizens; i++) {
         
         i = i + primeNumber; 
     } else {
-        deliveroo.push(i);
+        delivery.push(i);
     }
 }
 
-console.log('Amount of deliveries:' + deliveroo.length);
+console.log('Amount of deliveries:' + delivery.length);
